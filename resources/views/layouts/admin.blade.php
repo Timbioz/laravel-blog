a32902ad343bb87a472581814ca414aec9fab60c
timbioz <volkovtim@gmail.com>
2018-12-03 16:14:54 +0300

    Stage

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,21 +8,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
     <!-- Bootstrap CSS-->
-    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('static/vendor/bootstrap/css/bootstrap.min.css') }}">
     <!-- Font Awesome CSS-->
-    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('static/vendor/font-awesome/css/font-awesome.min.css') }}">
     <!-- Fontastic Custom icon font-->
-    <link rel="stylesheet" href="css/fontastic.css">
+    <link rel="stylesheet" href="{{ asset('static/css/fontastic.css') }}">
     <!-- Google fonts - Roboto -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
     <!-- jQuery Circle-->
-    <link rel="stylesheet" href="css/grasp_mobile_progress_circle-1.0.0.min.css">
+    <link rel="stylesheet" href="{{ asset('static/css/grasp_mobile_progress_circle-1.0.0.min.css') }}">
     <!-- Custom Scrollbar-->
-    <link rel="stylesheet" href="vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="{{ asset('static/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.css') }}">
     <!-- theme stylesheet-->
-    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <link rel="stylesheet" href="{{ asset('static/css/style.default.css') }}">
     <!-- Custom stylesheet - for your changes-->
-    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="{{ asset('static/css/custom.css') }}">
     <!-- Favicon-->
     <link rel="shortcut icon" href="img/favicon.ico">
     <!-- Tweaks for older IEs--><!--[if lt IE 9]>
@@ -559,16 +559,16 @@
     </footer>
 </div>
 <!-- JavaScript files-->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/popper.js/umd/popper.min.js"> </script>
-<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="js/grasp_mobile_progress_circle-1.0.0.min.js"></script>
-<script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
-<script src="vendor/chart.js/Chart.min.js"></script>
-<script src="vendor/jquery-validation/jquery.validate.min.js"></script>
-<script src="vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js"></script>
-<script src="js/charts-home.js"></script>
+<script src="{{ asset('static/vendor/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('static/vendor/popper.js/umd/popper.min.js') }}"></script>
+<script src="{{ asset('static/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('static/js/grasp_mobile_progress_circle-1.0.0.min.js') }}"></script>
+<script src="{{ asset('static/vendor/jquery.cookie/jquery.cookie.js') }}"></script>
+<script src="{{ asset('static/vendor/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('static/vendor/jquery-validation/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('static/vendor/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+<script src="{{ asset('static/js/charts-home.js') }}"></script>
 <!-- Main File-->
-<script src="js/front.js"></script>
+<script src="{{ asset('static/js/front.js') }}"></script>
 </body>
 </html>
