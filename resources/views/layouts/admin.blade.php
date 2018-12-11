ff1e1629472857932e6ece8565c996135c1f7ff1
timbioz <volkovtim@gmail.com>
2018-12-11 15:21:50 +0300

    Small fixes: admin.blade

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,12 +25,12 @@
                     <h2 class="h5">SCICENTER</h2><span>Tech & Science</span>
                 </div>
                 <!-- Small Brand information, appears on minimized sidebar-->
-                <div class="sidenav-header-logo"><a href="index.html" class="brand-small text-center"> <strong>S</strong><strong class="text-primary">C</strong></a></div>
+                <div class="sidenav-header-logo"><a href="{{ url('/') }}" class="brand-small text-center"> <strong>S</strong><strong class="text-primary">C</strong></a></div>
             </div>
             <!-- Sidebar Navigation Menus-->
             <div class="main-menu">
                 <ul id="side-main-menu" class="side-menu list-unstyled">
-                    <li><a href="index.html"> <i class="icon-home"></i>Home</a></li>
+                    <li><a href="{{ url('/') }}"> <i class="icon-home"></i>Home</a></li>
                     <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-interface-windows"></i>Posts</a>
                         <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
                         <li><a href="{{ route("admin.posts.create") }}">Create</a></li>
@@ -49,7 +49,7 @@
                 <div class="container-fluid">
                     <div class="navbar-holder d-flex align-items-center justify-content-between">
                         <div class="navbar-header"><a id="toggle-btn" href="#" class="menu-btn"><i class="icon-bars"> </i></a>
-                            <a href="index.html" class="navbar-brand">
+                            <a href="{{ url('/') }}" class="navbar-brand">
                                 <div class="brand-text d-none d-md-inline-block"><span>Bootstrap </span></div>
                             </a>
                         </div>
@@ -125,7 +125,7 @@
                                 </ul>
                             </li>
                             <!-- Log out-->
-                            <li class="nav-item"><a href="login.html" class="nav-link logout"> <span class="d-none d-sm-inline-block">Logout</span><i class="fa fa-sign-out"></i></a></li>
+                            <li class="nav-item"><a href="#" class="nav-link logout"> <span class="d-none d-sm-inline-block">Logout</span><i class="fa fa-sign-out"></i></a></li>
                         </ul>
                     </div>
                 </div>
@@ -141,8 +141,7 @@
                         <p>Your company &copy; 2017-2019</p>
                     </div>
                     <div class="col-sm-6 text-right">
-                        <p>Design by <a href="https://bootstrapious.com" class="external">Bootstrapious</a></p>
-                        <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions and it helps me to run Bootstrapious. Thank you for understanding :)-->
+                        <p>Design by <a href="https://bootstrapious.com" class="external">Amber</a></p>
                     </div>
                 </div>
             </div>
