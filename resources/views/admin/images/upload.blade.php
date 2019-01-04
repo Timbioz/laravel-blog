da652628d6ad09171033c820a26219bb752ff540
timbioz <volkovtim@gmail.com>
2019-01-04 17:10:14 +0300

    Removed dropzone and do image upload in vanilla js

--- resources/views/admin/images/upload.blade.php
+++ resources/views/admin/images/upload.blade.php
@@ -4,11 +4,16 @@
 
     <h1>Add New Image</h1>
     <hr>
-    <form action="{{ route('admin.images.store') }}" class="" method="post" id="images">
+    <form action="{{ route('admin.images.store') }}" class="dropzone" method="post" id="images">
         @csrf
-        <div id="images-upload" style="height: 200px; width: 200px; background-color: #ced2bf;"></div> <!-- this is were the previews should be shown. -->
-        <div id="container" style="width: 100px; height: 100px; background-color: #b1aba9"></div>
 
+        <div class="form-group">
+            <label for="image-upload">Title</label>
+            <input type="file" class="form-control" id="image-upload" name="image">
+        </div>
+
+        <button type="button" class="btn btn-secondary" id="ter">Remove</button>
+        <img id="blah" src="#" style="display: none" alt="your image" />
 
         <div class="form-group">
             <label for="post-title">Title</label>
@@ -27,116 +32,34 @@
         <button type="submit" class="btn btn-primary">Save</button>
     </form>
 
-
-
-    {{--<div id="tpl">--}}
-        {{--<div class="dz-preview dz-file-preview">--}}
-            {{--<div class="dz-details">--}}
-                {{--<div class="dz-filename"><span data-dz-name></span></div>--}}
-                {{--<div class="dz-size" data-dz-size></div>--}}
-                {{--<img data-dz-thumbnail />--}}
-            {{--</div>--}}
-            {{--<div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>--}}
-            {{--<div class="dz-success-mark"><span>✔</span></div>--}}
-            {{--<div class="dz-error-mark"><span>✘</span></div>--}}
-            {{--<div class="dz-error-message"><span data-dz-errormessage></span></div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-
-    <div id="container" style="width: 100px; height: 100px; background-color: #a9dba9"></div>
-
-    <div class="d-none">
-    <div id="tpl">
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" alt="Card image cap" data-dz-thumbnail>
-            <div class="card-body">
-                <h5 class="card-title" data-dz-name></h5>
-                <div class="dz-progress" style="height: 5px; background-color: #0e90d2"><span class="dz-upload" data-dz-uploadprogress></span></div>
-                <p class="card-text" data-dz-size></p>
-                <a href="#" class="btn btn-primary" data-dz-remove>Remove</a>
-            </div>
-        </div>
-    </div>
-    </div>
-
-
-
-
-
     @push('view_scripts')
+        <script>
 
-        <script type="application/javascript">
+            let imagePreview = document.querySelector('#blah');
+            let inputImage = document.getElementById('image-upload');
 
-            // window.onload = function () {
-            //     var progressBar = new ProgressBar.Circle('#container', {
-            //         strokeWidth: 2
-            //     });
-            //     progressBar.animate(0.3, {
-            //         duration: 2000
-            //     }, function() {
-            //         console.log('Animation has finished');
-            //     });
-            // };
+            function readURL(input) {
+                if (input.files && input.files[0]) {
+                    let reader = new FileReader();
+                    reader.onload = function(e) {
+                        imagePreview.src = e.target.result;
+                        imagePreview.style.display = 'block';
+                    };
 
-            // jQuery(document).ready(function() {
-            //
-            //     let form2 = $("div#my-awesome-dropzone").dropzone({
-            //         url: "/uploadcar",
-            //         thumbnailWidth: 240,
-            //         thumbnailHeight: 240,
-            //         previewTemplate: document.querySelector('#tpl').innerHTML,
-            //         addRemoveLinks: true,
-            //         uploadprogress: function (file, progress, bytesSent) {
-            //             alert(progress)
-            //         }
-            //     });
-            //
-            //
-            //
-            //
-            // });
+                    reader.readAsDataURL(input.files[0]);
+                }
+            }
 
+            document.getElementById('ter').addEventListener('click', function () {
+                inputImage.value = '';
+                imagePreview.src = '#';
+                imagePreview.style.display = 'none';
+            });
 
+            document.getElementById('image-upload').addEventListener('change', function () {
+                readURL(this);
+            });
 
-            // $(function() {
-            //     Dropzone.options.images = {
-            //         autoProcessQueue: false,
-            //         uploadMultiple: true,
-            //         parallelUploads: 100,
-            //         maxFiles: 3,
-            //         thumbnailWidth: 240,
-            //         thumbnailHeight: 240,
-            //         addRemoveLinks: true,
-            //
-            //         // The setting up of the dropzone
-            //         init: function () {
-            //             let myDropzone = this;
-            //
-            //             // First change the button to actually tell Dropzone to process the queue.
-            //             this.element.querySelector("button[type=submit]").addEventListener("click", function (e) {
-            //                 // Make sure that the form isn't actually being sent.
-            //                 e.preventDefault();
-            //                 e.stopPropagation();
-            //                 myDropzone.processQueue();
-            //             });
-            //
-            //             // Listen to the sendingmultiple event. In this case, it's the sendingmultiple event instead
-            //             // of the sending event because uploadMultiple is set to true.
-            //             this.on("sendingmultiple", function () {
-            //                 // Gets triggered when the form is actually being sent.
-            //                 // Hide the success button or the complete form.
-            //             });
-            //             this.on("successmultiple", function (files, response) {
-            //                 // Gets triggered when the files have successfully been sent.
-            //                 // Redirect user or notify of success.
-            //             });
-            //             this.on("errormultiple", function (files, response) {
-            //                 // Gets triggered when there was an error sending the files.
-            //                 // Maybe show form again, and notify user of error
-            //             });
-            //         }
-            //     };
-            // });
         </script>
     @endpush
 
